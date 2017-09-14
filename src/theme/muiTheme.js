b1949c1b158ccb4c153b7d2772a49b5089a95fd8
ManhLe <lequocmanh2010@gmail.com>
2017-09-15 00:02:24 +0700

    finished layout

--- src/theme/muiTheme.js
+++ src/theme/muiTheme.js
@@ -1,11 +1,9 @@
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import { teal500, grey50, grey900, tealA700 } from 'material-ui/styles/colors';
+import { deepOrange500 } from 'material-ui/styles/colors';
 
 const muiTheme = getMuiTheme({
   palette: {
-    primary1Color: tealA700,
-    textColor: grey50,
-    pickerHeaderColor: tealA700,
+    primary1Color: deepOrange500,
   },
 });
 
