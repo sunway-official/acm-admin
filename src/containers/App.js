e1c436a45852720f7a3d5e6bb6ad9bfc45a63c1f
thanhvinhlu <luthanhvinhdtu@gmail.com>
2017-09-16 20:26:38 +0700

    rewrite login ui

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,11 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 import CustomRoute from './CustomRoute';
 import Home from '../pages/Home';
 import NoMatch from '../pages/NoMatch';
+import Login from '../pages/Login/index';
 
-export default () =>
+export default () => (
   <div>
     <Switch>
       <CustomRoute exact path="/" component={Home} />
       <Route component={NoMatch} />
+      <Route path="/login" component={Login} />
     </Switch>
-  </div>;
+  </div>
+);
---
+++ src/pages/Login/index.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+import submit from './submit';
+import TextField from 'material-ui/TextField';
+import './style.css';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const renderField = ({ input, label, type, meta: { touched, error } }) => (
+  <div>
+    <div>
+      <TextField
+        {...input}
+        hintText={label}
+        floatingLabelText={label}
+        type={type}
+      />
+      {touched && error && <div> {error} </div>}
+    </div>
+  </div>
+);
+const Login = props => {
+  const { error, handleSubmit, submitting } = props;
+  return (
+    <div className="card" id="form-container">
+      <div className="card-content">
+        <h1 id="login-title">Login</h1>
+        <form onSubmit={handleSubmit(submit)}>
+          <Field
+            className="field"
+            name="username"
+            type="text"
+            component={renderField}
+            label="Username"
+          />
+          <Field
+            className="field"
+            name="password"
+            type="password"
+            component={renderField}
+            label="Password"
+          />
+          {error && <strong> {error} </strong>}
+          <div>
+            <RaisedButton
+              className="btn login"
+              disabled={submitting}
+              type="submit"
+              label="Log In"
+              labelPosition="before"
+              primary={true}
+            />
+          </div>
+        </form>
+        <div className="final-row">
+          <a className="forgot-password" href="/">
+            Forgot Password
+          </a>
+          <a className="register" href="/register">
+            Register
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default reduxForm({
+  form: 'submitValidation', // a unique identifier for this form
+})(Login);
