1831abf4b126c231e39014dcb9889deff86cfcc8
ManhLe <lequocmanh2010@gmail.com>
2017-09-18 16:59:31 +0700

    Refactor code and design Conference Information GUI

--- src/pages/conference/info/index.js
+++ src/pages/conference/info/index.js
@@ -1,14 +1,56 @@
-import React, { Component } from 'react';
-import style from './style.css';
+import React from 'react';
+import { TextField, DatePicker, TimePicker } from 'material-ui';
 
-class Index extends Component {
+export default class Index extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: 'Google Showcase',
+      description: 'Description',
+      place: 'Duy Tan University',
+    };
+  }
+
+  handleChange = event => {
+    this.setState({
+      title: event.target.title,
+      description: event.target.description,
+      place: event.target.place,
+    });
+  };
+
   render() {
     return (
       <div>
-        <style dangerouslySetInnerHTML={{ __html: style }} />
-        <div>Info page</div>
+        <div>
+          <TextField
+            id="title-controlled"
+            value={this.state.title}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <TextField
+            id="description-controlled"
+            value={this.state.description}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <TextField
+            id="place-controlled"
+            value={this.state.place}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <TimePicker hintText="Time of Conference" autoOk={true} />
+        </div>
+        <div>
+          <DatePicker hintText="Date of Conference" autoOk={true} />
+        </div>
       </div>
     );
   }
 }
-export default Index;
