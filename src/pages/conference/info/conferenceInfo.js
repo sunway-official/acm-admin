f00629f2d59907678c2a3e94e43dc55b55a3b57c
Tran Van Thuc <tranvanthucduytan@gmail.com>
2017-09-28 07:39:08 +0700

    Test mutation update Conference , update start date and end day wrong !

--- src/pages/conference/info/conferenceInfo.js
+++ src/pages/conference/info/conferenceInfo.js
@@ -1,36 +1,53 @@
 import React, { Component } from 'react';
-import { reduxForm } from 'redux-form';
+import { reduxForm, formValueSelector } from 'redux-form';
 import { RaisedButton } from 'material-ui';
 import { style } from './style.css';
-import CoOrganizer from './coOrganizerInfo';
-import Organizer from './organizerInfo';
+// import CoOrganizer from './coOrganizerInfo';
+// import Organizer from './organizerInfo';
 import BasicInfo from './basicInfo';
 import { connect } from 'react-redux';
-import { graphql, gql, compose } from 'react-apollo';
-import { conferenceOperations } from '../../../store/ducks/conference';
+import { gql, graphql, compose } from 'react-apollo';
 
 class Info extends Component {
-  componentDidMount() {
-    // console.log(this.props.conference.conference);
+  constructor() {
+    super();
+    this.saveConference = this.saveConference.bind(this);
+  }
+
+  saveConference() {
+    const { mutate, id, title, description, startDate, endDate } = this.props;
+    console.log(startDate);
+    console.log(endDate);
+    mutate({
+      variables: {
+        id: id,
+        title: title,
+        description: description,
+        start_date: startDate,
+        end_date: endDate,
+      },
+    });
     // console.log(this.props);
   }
 
   render() {
     const { handleSubmit, submitting, pristine } = this.props;
+
     return (
       <form className="form conference-info" onSubmit={handleSubmit}>
         <div>
           <style dangerouslySetInnerHTML={{ __html: style }} />
           <div>
             <BasicInfo />
-            <Organizer />
-            <CoOrganizer />
+            {/* <Organizer />
+            <CoOrganizer /> */}
             <div className="d-flex save-btn btn-group">
               <RaisedButton
                 label="Save"
                 primary={true}
                 type="submit"
                 disabled={pristine || submitting}
+                onClick={this.saveConference}
               />
             </div>
           </div>
@@ -39,25 +56,15 @@ class Info extends Component {
     );
   }
 }
-const minDate = new Date();
+// const minDate = new Date();
 
 Info = reduxForm({
   form: 'conferenceInfo',
-  initialValues: {
-    title: '12321',
-    description: '123',
-    startDate: minDate,
-    organizerName: 'Duy Tan University',
-    organizerEmail: 'duytan@gmail.com',
-    organizerWebsite: 'mydtu.com',
-    organizerPhoneNumber: '123-123-1233',
-  },
 })(Info);
 
 const mapStateToProps = (state, ownProps) => {
-  console.log(state);
   const conference = ownProps.conference;
-  const organizerDetail = conference.organizerDetail;
+  // const organizerDetail = conference.organizerDetail;
   return {
     id: state.conference.id,
     initialValues: {
@@ -65,12 +72,54 @@ const mapStateToProps = (state, ownProps) => {
       description: conference.description,
       startDate: new Date(conference.start_date),
       endDate: new Date(conference.end_date),
-      organizerName: organizerDetail.name,
-      organizerEmail: organizerDetail.email,
-      organizerWebsite: organizerDetail.website,
-      organizerPhoneNumber: organizerDetail.phone,
+      // organizerName: organizerDetail.name,
+      // organizerEmail: organizerDetail.email,
+      // organizerWebsite: organizerDetail.website,
+      // organizerPhoneNumber: organizerDetail.phone,
     },
   };
 };
 
-export default connect(mapStateToProps, undefined)(Info);
+const selector = formValueSelector('conferenceInfo'); // <-- same as form name
+Info = connect(state => {
+  // can select values individually
+  const title = selector(state, 'title');
+  const description = selector(state, 'description');
+  const startDate = selector(state, 'startDate');
+  const endDate = selector(state, 'endDate');
+  return {
+    title,
+    description,
+    startDate,
+    endDate,
+  };
+})(Info);
+
+const UPDATE_CONFERENCE = gql`
+  mutation UpdateConference(
+    $id: ID!
+    $title: String!
+    $description: String!
+    $start_date: Date!
+    $end_date: Date!
+  ) {
+    updateConference(
+      id: $id
+      title: $title
+      description: $description
+      start_date: $start_date
+      end_date: $end_date
+    ) {
+      id
+      title
+      description
+      start_date
+      end_date
+    }
+  }
+`;
+
+export default compose(
+  connect(mapStateToProps, undefined),
+  graphql(UPDATE_CONFERENCE),
+)(Info);
