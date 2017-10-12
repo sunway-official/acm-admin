e5a5148eebf499ef02d494c73bba4702e79feb01
ManhLe <lequocmanh2010@gmail.com>
2017-10-12 22:35:18 +0700

    create Edit activity GUI

--- src/pages/schedule/index.js
+++ src/pages/schedule/index.js
@@ -4,6 +4,9 @@ import BigCalendar from 'react-big-calendar';
 import moment from 'moment';
 import events from './events';
 import AddActivity from './addActivity';
+import EditActivity from './editActivity';
+import { Dialog, IconButton } from 'material-ui';
+import { NavigationClose } from 'material-ui/svg-icons';
 
 import {
   getEvents,
@@ -20,21 +23,29 @@ const style = {
 
 BigCalendar.momentLocalizer(moment);
 
-class MyCalendar extends React.Component {
+class MyCalendar extends React.PureComponent {
   constructor(props) {
     super(props);
 
+    this.handleEdit = this.handleEdit.bind(this);
     this.state = {
       events: events,
+      openEdit: false,
     };
     this.submit = this.submit.bind(this);
   }
+  handleEdit(events) {
+    this.setState({ openEdit: true, events: events });
+  }
+
+  handleClose = () => {
+    this.setState({ openEdit: false });
+  };
 
   submit(values) {
     const { INSERT_ACTIVITY_MUTATION, INSERT_SCHEDULE_MUTATION } = this.props;
 
     const conferenceId = this.props.match.params.id;
-    console.log(values);
 
     INSERT_ACTIVITY_MUTATION({
       variables: {
@@ -78,12 +89,24 @@ class MyCalendar extends React.Component {
     return (
       <div style={style}>
         <AddActivity onSubmit={this.submit} />
+        <Dialog open={this.state.openEdit}>
+          <EditActivity events={this.state.events} />
+          <IconButton
+            tooltip="Close"
+            className="cancel-btn dialog"
+            onClick={this.handleClose}
+          >
+            <NavigationClose />
+          </IconButton>
+        </Dialog>
         <BigCalendar
           popup
           events={events.concat(myEvents)}
           defaultView="week"
           defaultDate={new Date()}
-          onSelectEvent={event => console.log(event)}
+          onSelectEvent={events => {
+            this.handleEdit(events);
+          }}
         />
       </div>
     );
