ba8930a97f8ab24b8d1eeca75df787a694ac111e
Nhat Khanh <lednhatkhanh@gmail.com>
2017-11-20 23:31:16 +0700

    Add file upload, change user avatar!

---
+++ src/lib/s3.js
@@ -0,0 +1,43 @@
+import AWS from 'aws-sdk/dist/aws-sdk-react-native';
+import { Buffer } from 'buffer';
+import uuid from 'uuid/v1';
+
+const S3_ACCESS_KEY = process.env.REACT_APP_S3_ACCESS_KEY;
+const S3_SECRET_KEY = process.env.REACT_APP_S3_SECRET_KEY;
+const S3_BUCKET_NAME = process.env.REACT_APP_S3_BUCKET_NAME;
+const S3_BUCKET_REGION = process.env.REACT_APP_S3_BUCKET_REGION;
+
+const S3 = new AWS.S3({
+  region: S3_BUCKET_REGION,
+  credentials: {
+    accessKeyId: S3_ACCESS_KEY,
+    secretAccessKey: S3_SECRET_KEY,
+  },
+});
+
+export const putAsync = ({ name, base64, ...options }) => {
+  return new Promise((resolve, reject) => {
+    let uriParts = name.split('.');
+    let fileType = uriParts[uriParts.length - 1];
+
+    const params = {
+      Bucket: S3_BUCKET_NAME,
+      Key: uuid() + '.' + fileType, // Generate random UUID
+      Body: new Buffer(base64, 'base64'), // Tranform file to base64
+      ACL: 'public-read', // By default: putObject will set file to public
+      ...options,
+    };
+
+    S3.putObject(params, (err, data) => {
+      if (err) {
+        reject(err);
+      }
+      resolve({
+        Key: params.Key,
+        response: data,
+      });
+    });
+  });
+};
+
+export default { putAsync };
