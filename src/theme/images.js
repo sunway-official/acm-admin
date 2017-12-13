3217fdd76ca08b15003fd86386bc5f4928340903
ManhLe <lequocmanh2010@gmail.com>
2017-12-13 19:55:05 +0700

    edit logo

--- src/theme/images.js
+++ src/theme/images.js
@@ -1,6 +1,7 @@
 export default {
   defaultAvatar: require('../images/avatar-1606916_640.png'),
   defaultLogo: require('../images/logo.png'),
+  defaultLogoSidebar: require('../images/logo-sidebar.png'),
   conferencerInfoBackground: require('../images/background/background1.jpg'),
   mapTemplate: require('../images/mapTemplate1.PNG'),
   backgroundimage: require('../images/background/backgroundimage.jpg'),
